b38c4b575e9e90108e0e0589522388a6f1ec6174
Matt Brophy <mattbroph@gmail.com>
2025-02-10 19:13:29 -0600

    added database table creation and test data load

---
+++ src/main/db/create_tables_bass_tracker.sql
@@ -0,0 +1,106 @@
+USE bass_tracker;
+
+#########################################################
+# Remove all tables if they exist, prior to creating them
+#########################################################
+DROP TABLE IF EXISTS Journal;
+DROP TABLE IF EXISTS Wind;
+DROP TABLE IF EXISTS Weather;
+DROP TABLE IF EXISTS Method;
+DROP TABLE IF EXISTS Lake;
+DROP TABLE IF EXISTS BassGoal;
+DROP TABLE IF EXISTS AppUser;
+
+#########################################################
+# Create initial tables
+#########################################################
+
+CREATE TABLE Journal (
+    JournalID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    UserID INTEGER NOT NULL,
+    JournalDate DATE NOT NULL,
+    LakeID INTEGER NOT NULL,
+    Hours INTEGER NOT NULL,
+    MethodID INTEGER NOT NULL,
+    AirTemp INTEGER NOT NULL,
+    WeatherID INTEGER NOT NULL,
+    WindID INTEGER NOT NULL,
+    Comments TEXT,
+    ImageURL varchar(255),
+    SM_14_16 INTEGER NOT NULL,
+    SM_16_19 INTEGER NOT NULL,
+    SM_19_PLUS INTEGER NOT NULL,
+    LM_14_16 INTEGER NOT NULL,
+	LM_16_19 INTEGER NOT NULL,
+    LM_19_PLUS INTEGER NOT NULL
+);
+
+CREATE TABLE Wind (
+    WindID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    WindType varchar(50) NOT NULL
+);
+
+CREATE TABLE Weather (
+    WeatherID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    WeatherType varchar(50) NOT NULL
+);
+
+CREATE TABLE Method (
+    MethodID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    MethodName varchar(50) NOT NULL
+);
+
+CREATE TABLE AppUser (
+    UserID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    UserName varchar(50) NOT NULL,
+    FirstName varchar(50) NOT NULL,
+    LastName varchar(50) NOT NULL,
+    ProfilePicture varchar(255)
+);
+
+CREATE TABLE BassGoal (
+    UserID INTEGER NOT NULL,
+    GoalYear YEAR NOT NULL,
+    GoalCount int NOT NULL,
+    PRIMARY KEY (UserID, GoalYear)
+);
+
+CREATE TABLE Lake (
+    LakeID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    LakeName varchar(50) NOT NULL,
+    UserID INTEGER NOT NULL
+);
+
+#########################################################
+# Create Foreign Key Constraints
+#########################################################
+
+ALTER TABLE BassGoal
+ADD CONSTRAINT FK_BassGoal_User FOREIGN KEY (UserID)
+	REFERENCES AppUser(UserID)
+    ON DELETE CASCADE; -- If user is deleted, then bassgoal is deleted 
+    
+ALTER TABLE Lake
+ADD CONSTRAINT FK_Lake_User FOREIGN KEY (UserID)
+	REFERENCES AppUser(UserID)
+    ON DELETE CASCADE; -- If user is deleted, then lakes are deleted
+
+ALTER TABLE Journal
+ADD CONSTRAINT FK_Wind FOREIGN KEY (WindID)
+	REFERENCES Wind(WindID);
+
+ALTER TABLE Journal
+ADD CONSTRAINT FK_Weather FOREIGN KEY (WeatherID)
+	REFERENCES Weather(WeatherID);
+    
+ALTER TABLE Journal
+ADD CONSTRAINT FK_Method FOREIGN KEY (MethodID)
+	REFERENCES Method(MethodID);
+    
+ALTER TABLE Journal
+ADD CONSTRAINT FK_Journal_User FOREIGN KEY (UserID)
+	REFERENCES AppUser(UserID);
+
+ALTER TABLE Journal
+ADD CONSTRAINT FK_Lake FOREIGN KEY (LakeID)
+	REFERENCES Lake(LakeID);
---
+++ src/main/db/insert_tables_bass_tracker.sql
@@ -0,0 +1,93 @@
+USE bass_tracker;
+
+################################
+# WIND
+################################
+INSERT INTO Wind (WindType)
+VALUES ("0-5 mph"),
+       ("5-10 mph"),
+       ("10-15 mph"),
+       ("15-20 mph"),
+       ("20+ mph");
+
+################################
+# Weather
+################################
+INSERT INTO Weather (WeatherType)
+VALUES ("Sunny"),
+       ("Partly Sunny"),
+       ("Cloudy"),
+       ("Partly Cloudy"),
+       ("Rain");
+
+################################
+# Method
+###############################
+INSERT INTO Method (MethodName)
+VALUES ("Crankbaits"),
+       ("Shoreline fishing"),
+       ("Bed fishing"),
+       ("Ned rig"),
+       ("Wacky worm"),
+       ("Lindy rig"),
+       ("Swim bait"),
+       ("Swim jig"),
+       ("Drop shot"),
+       ("Crib fishing"),
+       ("Fly fishing"),
+       ("Other");
+	
+################################
+# AppUser
+################################
+INSERT INTO AppUser (UserName, FirstName, LastName, ProfilePicture)
+VALUES ("MattyB", "Matt", "Brophy", "https://urltomattspic"),
+	   ("JohnnyD", "John", "Brophy", "https://urltojohnspic"),
+       ("FrodoB", "Frodo", "Baggins", "https://urltofrodospic");
+       
+################################
+# Lake
+################################
+INSERT INTO Lake (LakeName, UserID)
+VALUES ("Lake Kegonsa", "1"),
+	   ("Lake Mendota", "1"),
+       ("Lake Monona", "1"),
+       ("Boom Lake", "2"),
+       ("Lake Flannery", "2"),
+       ("Lake Michigan", "3");
+
+################################
+# Bass Goal
+################################
+INSERT INTO BassGoal (UserID, GoalYear, GoalCount)
+VALUES ("1", "2025", "65"),
+       ("1", "2024", "120"),
+	   ("2", "2025", "90"),
+       ("3", "2025", "32");
+
+################################
+# Journal
+################################
+INSERT INTO Journal (UserID, JournalDate, LakeID, Hours, MethodID, AirTemp, WeatherID, WindID, Comments, ImageURL, SM_14_16, SM_16_19, SM_19_PLUS, LM_14_16, LM_16_19, LM_19_PLUS)
+VALUES ("1", "2025-02-25", "2", "5", "2", "80", "2", "2", "Had a really good time fishing today", "https://myimage.com", "2", "3", "4", "8", "1", "0"),
+	   ("1", "2025-02-24", "1", "2", "3", "75", "1", "1", "Had so much fun", "https://myimage2.com", "1", "1", "0", "0", "1", "0"),
+       ("2", "2025-02-24", "4", "2", "3", "62", "3", "4", "What a day", "https://myimage3.com", "1", "11", "0", "10", "1", "0"),
+       ("3", "2025-02-24", "6", "2", "3", "90", "4", "3", "Today was rough", "https://myimage4.com", "1", "1", "0", "0", "6", "9");
+
+################################
+# Check your queries
+################################
+SELECT FirstName, LastName, LakeName, Hours, MethodName, AirTemp, WeatherType, WindType, Comments, ImageURL, SM_14_16, SM_16_19, SM_19_PLUS, LM_14_16, LM_16_19, LM_19_PLUS
+FROM Journal
+INNER JOIN AppUSer ON Journal.UserID = AppUser.UserID
+INNER JOIN Lake ON Journal.LakeID = Lake.LakeID
+INNER JOIN Method ON Journal.MethodID = Method.MethodID
+INNER JOIN Weather ON Journal.WeatherID = Weather.WeatherID
+INNER JOIN Wind ON Journal.WindID = Wind.WindID;
+
+
+
+
+
+
+
