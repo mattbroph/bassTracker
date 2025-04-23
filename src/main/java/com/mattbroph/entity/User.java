bfcaf61b7dca88bcf3648b2862879a56e7b9a649
Matt Brophy <mattbroph@gmail.com>
2025-04-23 13:23:27 -0500

    added hibernate validator to edit profile

--- src/main/java/com/mattbroph/entity/User.java
+++ src/main/java/com/mattbroph/entity/User.java
@@ -2,9 +2,9 @@ package com.mattbroph.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
